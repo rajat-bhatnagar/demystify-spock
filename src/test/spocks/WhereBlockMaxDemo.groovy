ea2dc5182df10286b3fe20e95f6ab207846519c1
rajatBhatnagar <rajat_bhatnagar@outlook.com>
2016-01-17 21:04:38 -0500

    Added class to demonstrate data tables using where block

---
+++ src/test/spocks/WhereBlockMaxDemo.groovy
@@ -0,0 +1,20 @@
+package spocks
+
+import spock.lang.Specification
+
+/**
+ * Created by rajatbhatnagar on 1/17/16.
+ */
+class WhereBlockMaxDemo extends Specification {
+
+    def "find max number using two sets of data"(int a , int b , int c){
+        expect:
+            Math.max(a,b) == c
+            println 'max ' + c
+        where:
+            a | b | c
+            1 | 3 | 3
+            9 | 2 | 9
+            8 | 2 | 8
+    }
+}
